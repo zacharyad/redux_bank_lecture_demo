8b1fed548e4f043d4fb098246527fdc4b78f0ffc
Cássio Zen <cassiozen@gmail.com>
2018-01-31 12:50:50 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const balance = document.getElementById("balance");
+const deposit5 = document.getElementById("deposit5");
+const deposit25 = document.getElementById("deposit25");
+const withdraw5 = document.getElementById("withdraw5");
+const withdraw25 = document.getElementById("withdraw25");
+
+deposit5.onclick = () => console.log("Deposit 5");
+deposit25.onclick = () => console.log("Deposit 25");
+withdraw5.onclick = () => console.log("Withdraw 5");
+withdraw25.onclick = () => console.log("Withdraw 25");
